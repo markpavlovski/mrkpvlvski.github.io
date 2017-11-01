f8c342f78d0adecd899f751cbaf2e940e4efbd92
Mark Pavlovski <mark.pavlovski@milliman.com>
2017-11-01 16:00:50 -0700

    cleaned up file

--- javascript/Node Test/hello.js
+++ javascript/Node Test/hello.js
@@ -13,3 +13,17 @@ request(url, (error, response, body)=> {
     console.log("Got an error: ", error, ", status code: ", response.statusCode)
   }
 })
+
+require('plotly')("mrkpvlvski", "bkNoojjCvRblqrtyTUdi");
+
+var data = [
+  {
+    x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
+    y: [1, 3, 6],
+    type: "scatter"
+  }
+];
+var graphOptions = {filename: "date-axes", fileopt: "overwrite"};
+plotly.plot(data, graphOptions, function (err, msg) {
+    console.log(msg);
+});
