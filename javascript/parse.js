0813ba1a00dc1bc8f9eacc9c02a4f64fbff67d53
Mark Pavlovski <mark.pavlovski@milliman.com>
2017-11-03 15:26:39 -0700

    Added parse code

---
+++ javascript/parse.js
@@ -0,0 +1,38 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var req = new XMLHttpRequest(); 
+var htmlString; 
+req.open('GET', 'https://github.com/mrkpvlvski/mrkpvlvski.github.io/tree/master/byExample/js', false);   
+req.send(null);  
+if(req.status == 200) {
+	htmlString = req.responseText;
+	//console.log(req.responseText);
+}
+
+//console.log(htmlString);
+
+var myString = "hello, how are you how?";
+var ind;
+ind = myString.indexOf("how", 20);
+console.log(ind);
+
+var startingPositions = [];
+var endPositions = [];
+ind = 0;
+while (ind !== -1){
+	ind++;
+	ind = htmlString.indexOf("/mrkpvlvski/mrkpvlvski.github.io/blob/master/byExample/js/", ind);
+	startingPositions.push(ind);
+	endPositions.push(htmlString.indexOf("\"", ind))
+} 
+console.log(startingPositions.slice(0,-1))
+console.log(endPositions.slice(0,-1))
+//console.log("https://github.com/" + htmlString.substr(startingPositions[0],endPositions[0]-startingPositions[0]))
+
+var links =[];
+for (var i = 0; i < startingPositions.length - 1; i++){
+	links.push("https://github.com/" + htmlString.substr(startingPositions[i],endPositions[i]-startingPositions[i]))
+	console.log(links[i])
+}
+
+
+//JQuery
