7f62e035ebc7d35c5eb7eef11386e4aed2299cce
Mark Pavlovski <markpavlovski@gmail.com>
2017-11-10 16:43:35 -0800

    updated google maps

--- javascript/Google Maps Elevation API/main.js
+++ javascript/Google Maps Elevation API/main.js
@@ -15,7 +15,7 @@ var loc = inputloc.split(", ");
 
 // Inputs:
 radius = 5;
-scale = 10; 
+scale = 1; 
 
 
 resolutionWidth = 0.001265; // Fremont city block length North-South
@@ -55,31 +55,50 @@ var elevator;
 elevations = [];
 vertices = [sampleSize];
 
+// function initMap() {
+// 	elevator = new google.maps.ElevationService;
+// 	for (var i = 0; i < sampleSize; i++ ){
+// 		elevator.getElevationForLocations({
+// 		    'locations': [requestInputs[i]]
+// 		}, function(results, status) {
+// 			if (status === 'OK') {
+// 				myElevation.elevation  = results[0].elevation;
+// 				myElevation.status = status;
+// 				myElevation.latlng = results[0].location;
+// 				myElevation.resolution = results[0].resolution;
+
+// 				console.log("The elevation at this point is " + results[0].elevation + " meters.");
+// 				elevations.push(results[0].elevation)
+// 				vertices.push({
+// 					lat: requestInputs[vertices.length-1].lat,
+// 					lng: requestInputs[vertices.length-1].lng,
+// 					elv: results[0].elevation
+// 				})
+
+// 			} else {
+// 				console.log("Elevation service failed due to: " + status);
+// 			}
+// 		});
+// 	}
+// }
+
+
+
 function initMap() {
 	elevator = new google.maps.ElevationService;
 	for (var i = 0; i < sampleSize; i++ ){
 		elevator.getElevationForLocations({
-		    'locations': [requestInputs[i]]
+		    'locations': requestInputs
 		}, function(results, status) {
 			if (status === 'OK') {
-				myElevation.elevation  = results[0].elevation;
-				myElevation.status = status;
-				myElevation.latlng = results[0].location;
-				myElevation.resolution = results[0].resolution;
-
-				console.log("The elevation at this point is " + results[0].elevation + " meters.");
-				elevations.push(results[0].elevation)
-				vertices.push({
-					lat: requestInputs[vertices.length-1].lat,
-					lng: requestInputs[vertices.length-1].lng,
-					elv: results[0].elevation
-				})
-
+				for (var i = 0; i < sampleSize; i++){
+					console.log("The point is " + requestInputs[i].lat + ",  " + requestInputs[i].lng);
+					console.log("The elevation at this point is " + results[i].elevation + " meters.");
+					vertices.push([requestInputs[i].lng,requestInputs[i].lat,results[i].elevation])
+				}
 			} else {
 				console.log("Elevation service failed due to: " + status);
 			}
 		});
 	}
 }
-
-
