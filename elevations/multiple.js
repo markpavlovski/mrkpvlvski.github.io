10a226b99ef61d48e595fc82de67fa2c3b2988d2
Mark Pavlovski <markpavlovski@gmail.com>
2017-11-16 16:27:37 -0800

    managed to get multiple requests working. Next step is to generate teh array of anchor positions

--- elevations/multiple.js
+++ elevations/multiple.js
@@ -16,37 +16,9 @@ function Shade(pct) {
 }
 
 // Get User Inputs
-var inputloc = prompt("Please enter coordinates (default is Lighthouse Roasters in Fremont):", "47.659064, -122.354199");
+var inputloc0 = prompt("Please enter coordinates (default is Lighthouse Roasters in Fremont):", "47.659064, -122.354199");
 var scale = prompt("Please input scale: (1 corresponds to the length of one block N-S)", 1);
-var loc = inputloc.split(", ");
-
-
-// Defaults
-var radius = 10;
-var resolutionWidth = 0.001265; // Fremont city block length North-South
-
-// Stage Grid
-var step = resolutionWidth * scale;
-var sampleSize = ( 2 * radius + 1 ) ** 2;
-var anchorLocation = {
-	lat: parseFloat(loc[0]), 
-	lng: parseFloat(loc[1])
-};
-var topLeftLocation = {
-	lat: anchorLocation.lat + radius * step, 
-	lng: anchorLocation.lng - radius * step
-};
-var requestInputs = [];
-for ( var i = 0; i < 2 * radius + 1; i++) {
-	for (var j = 0; j < 2 * radius + 1; j ++){
-		requestInputs.push({
-				lat: topLeftLocation.lat - step * i , 
-				lng: topLeftLocation.lng + step * j, 
-		})
-	}
-}
-
-
+var inputArray = ["47.659064, -122.354199","47, -122"];
 
 container = document.getElementById('container');
 
@@ -55,7 +27,33 @@ var multipleResults = []
 
 
 
-function initMap() {
+function initMap(inputAnchor) {
+
+
+	var loc = inputAnchor.split(", ");
+	var radius = 10;
+	var resolutionWidth = 0.001265; // Fremont city block length North-South
+
+	// Stage Grid
+	var step = resolutionWidth * scale;
+	var sampleSize = ( 2 * radius + 1 ) ** 2;
+	var anchorLocation = {
+	lat: parseFloat(loc[0]), 
+	lng: parseFloat(loc[1])
+	};
+	var topLeftLocation = {
+	lat: anchorLocation.lat + radius * step, 
+	lng: anchorLocation.lng - radius * step
+	};
+	var requestInputs = [];
+	for ( var i = 0; i < 2 * radius + 1; i++) {
+	for (var j = 0; j < 2 * radius + 1; j ++){
+		requestInputs.push({
+				lat: topLeftLocation.lat - step * i , 
+				lng: topLeftLocation.lng + step * j, 
+		})
+	}
+	}
 
 	var elevator = new google.maps.ElevationService;
 	elevator.getElevationForLocations({'locations': requestInputs}, function(results, status) {
@@ -64,8 +62,9 @@ function initMap() {
 			//Create elevation table
 			for (var i = 0; i < sampleSize; i++){
 				elevations.push(results[i].elevation)
+				requestInputs[i].elv = results[i].elevation
 			}
-			multipleResults.push(elevations)
+			multipleResults.push(requestInputs)
 		} else {
 			console.log("Elevation service failed due to: " + status);
 		}
@@ -73,10 +72,14 @@ function initMap() {
 	});
 }
 
-var deltaTime = 3*1000;
+var deltaTime = 3000; // 3 seconds
 
-for (var i = 0; i < 9; i++){
-	setTimeout(function(){initMap();},i*deltaTime);
+var j = 0;
+for (var i = 0; i < inputArray.length; i++){
+	setTimeout(function(){
+		initMap(inputArray[j]);
+		j++;
+	},i*deltaTime);
 }
 
 	
