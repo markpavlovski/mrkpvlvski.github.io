23cb58a0b5e6e2be38ad14e593d79c05d5b8d57d
Mark Pavlovski <markpavlovski@gmail.com>
2017-12-28 12:38:29 -0800

    started adding multilabelmatrix

--- elevationmap/js/neighbours.js
+++ elevationmap/js/neighbours.js
@@ -110,7 +110,6 @@ class SingleLabelMatrix extends Matrix {
 		super(matrix.rows, matrix.columns);
 		this.inputData = matrix.data;
 		this.buffer = new Matrix(3);
-		this.buffer.setToValue(null);
 		this.parents = [];
 	}
 	setBuffer(k,l){
@@ -135,7 +134,7 @@ class SingleLabelMatrix extends Matrix {
 		}
 	}
 	setLabels(){
-		var log = document.getElementById("log");
+		// var log = document.getElementById("log"); //used for debugging only
 		for (var i = 0; i < this.rows; i++){
 			for (var j = 0; j < this.columns; j++){
 				if (this.inputData[i][j] === 1){
@@ -199,6 +198,102 @@ class SingleLabelMatrix extends Matrix {
 	}
 
 }
+
+
+class MultiLabelMatrix extends Matrix {
+	constructor(maxtrix){
+		super(matrix.rows, matrix.columns);
+		this.inputData = matrix.data;
+		this.buffer = new Matrix(3);
+		this.parents = [];
+	}
+	setBuffer(k,l,layer){
+		this.buffer.setToValue(null);
+		for (var i = 0; i < 3; i++){
+			for (var j = 0; j < 3; j++){
+				if ( k-1+i >= 0 && k-1+i <= this.rows -1  && l-1+j >= 0 && l-1+j <= this.columns -1 && this.inputData[k-1+i][l-1+j] >= layer){
+					this.buffer.data[i][j] = this.parents[this.data[k-1+i][l-1+j]];
+				}
+			}
+		}
+	}
+	isValidLocation(i,j,layer){
+		if ( i < 0 || j < 0 || i >= this.rows || j >= this.columns ){
+			return false;
+		} else if ( this.data[i][j] === null || this.data[i][j] === undefined ){
+			return false;
+		} else if ( this.inputData[i][j] < layer){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	setLabels(){
+		// var log = document.getElementById("log"); //used for debugging only
+		for (var i = 0; i < this.rows; i++){
+			for (var j = 0; j < this.columns; j++){
+				if (this.inputData[i][j] === 1){
+					this.setBuffer(i,j);
+					if (this.buffer.getMin() === null){
+						this.data[i][j] = this.parents.length;
+						this.parents.push(this.parents.length);
+					} else {
+						this.data[i][j] = this.parents[this.buffer.getMin()];
+						this.parents[this.data[i][j]] = this.parents[this.buffer.getMin()];
+						for (var m = 0; m < 3; m++){
+							for (var n = 0; n < 3; n++){
+								if ( this.isValidLocation(i-1+m, j-1+n) ){
+									this.parents[this.parents[this.data[i-1+m][j-1+n]]] = this.parents[this.buffer.getMin()];
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	optimizeParents(){
+		for (var i=0; i < this.parents.length; i++){
+			while (this.parents[this.parents[i]] !== this.parents[i]){
+				this.parents[i] = this.parents[this.parents[i]];
+			}
+		}
+	}
+	relabelParents(){
+		var uniqueParents = [];
+		for (var i=0; i < this.parents.length; i++){
+			if(uniqueParents.indexOf(this.parents[i]) === -1){
+				uniqueParents.push(this.parents[i])
+			}
+		}
+		for (var i=0; i < this.parents.length; i++){
+			this.parents[i] = uniqueParents.indexOf(this.parents[i])
+		}
+	}
+	relabelMatrix(){
+		for (var i = 0; i < this.rows; i++){
+			for (var j = 0; j < this.columns; j++){
+				if (this.data[i][j] === null){
+					this.data[i][j] = "x";
+				} else {
+					this.data[i][j] = this.parents[this.data[i][j]];
+				}
+			}
+		}
+	}
+	// getLabels method consolidates all steps into one and generates final labeled matrix.
+	getLabels(){
+		this.setLabels();
+		this.optimizeParents();
+		this.relabelParents();
+		this.relabelMatrix();
+		var outputMatrix = new Matrix(this.rows,this.columns);
+		outputMatrix.data = this.data;
+		return outputMatrix;
+	}
+
+}
+
 
 class ShadedTable {
 	constructor(matrix, labelMatrix, cellSize, targetDivId) {
